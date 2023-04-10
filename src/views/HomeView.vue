9557763a5e4de40f0c909d0c058bd49b600971d0
Victor <uberman59@gmail.com>
2023-04-10 23:44:31 +0500

    feat: homepage add img with routes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import sourceData from "@/data.json";
+const destinations = sourceData.destinations;
+</script>
 
 <template>
   <h1>Hello</h1>
+  <router-link
+    v-for="destination in destinations"
+    :key="destination.id"
+    :to="destination.slug"
+  >
+    <h2>{{ destination.name }}</h2>
+    <img :src="`/images/${destination.image}`" :alt="destination.name"
+  /></router-link>
 </template>
